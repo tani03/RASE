671cf5624f459b6acfa8a2643dc2b3bf196fc379
neerajamoli-145 <54245814+neerajamoli-145@users.noreply.github.com>
2020-11-29 16:28:35 +0530

    interface for database controller

---
+++ RASE/src/interfaces/IDBService.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import model.User;
+
+public interface IDBService {
+
+	public boolean createUser(User user); // return true if record is inserted without any exception. else false
+	public boolean emailExists(String email); // return true if email already exists in DB, else return false 
+	public User getUser(String email); // email is our primary key, fetch records from database, return null if not found
+}
