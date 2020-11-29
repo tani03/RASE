bed049149ea97a21d807dd929c8533f35745d7fe
alvinavarachan <53207740+alvinavarachan@users.noreply.github.com>
2020-11-29 23:11:24 +0530

    Add files via upload

---
+++ RASE/src/db/DbConnection.java
@@ -0,0 +1,27 @@
+package com.rase.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import oracle.jdbc.driver.OracleDriver;
+
+public class DbConnection {
+
+	
+	private static Connection connection=null;
+	
+	 public Connection get_conection() {
+	
+		 try {
+			DriverManager.registerDriver(new OracleDriver());
+			connection=  DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 return connection;
+	 }
+}
---
+++ RASE/src/db/IDBImplement.java
@@ -0,0 +1,101 @@
+package com.rase.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import interfaces.IDBService;
+import model.User;
+
+public class IDBImplement  implements IDBService{
+
+	
+	DbConnection dbcon = new DbConnection();
+	
+	Connection connection= dbcon.get_conection();
+	
+	
+	@Override
+	public boolean createUser(User user) {
+		
+		boolean returnValue = false;
+		try {
+			PreparedStatement prepStatement = connection.prepareStatement("insert into students values (?,?,?,?)");
+			prepStatement.setString(1, user.getEmail());
+			prepStatement.setString(2, user.getName());
+			prepStatement.setString(3, user.getPhone() );
+			prepStatement.setString(4,user.getPassword());
+			
+			if(prepStatement.executeUpdate() !=0) {	
+				returnValue = true;	
+			}
+			else {
+				returnValue =false;
+			}
+			connection.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return returnValue;
+	}
+
+		
+	@Override
+	public boolean emailExists(String email) {
+		
+		boolean returnValue = false;
+		try {
+			PreparedStatement prepStatement = connection.prepareStatement("select * from students where email_id = (?)");
+			prepStatement.setString(1, email);
+			ResultSet resultset = prepStatement.executeQuery();
+			
+			if(resultset.next() ) {	
+				returnValue=true;
+			}
+			else {
+				returnValue=false;
+			}
+			connection.close();	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return returnValue;
+	}
+
+	
+	@Override
+	public User getUser(String email) {
+		
+		User user = new User();
+		try {
+			PreparedStatement prepStatement = connection.prepareStatement("select * from students where email_id = (?)");
+			prepStatement.setString(1, email);
+			ResultSet resultset = prepStatement.executeQuery();
+			
+			if(resultset.next() ) {	
+				
+				user.setName(resultset.getString("first_name"));
+				user.setEmail(resultset.getString("email_id"));
+				user.setPhone(resultset.getString("phone"));
+				
+				
+			}
+			else {
+				user = null;
+			}
+			connection.close();	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return user;
+	}
+
+}
